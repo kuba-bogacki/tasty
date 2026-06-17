72a59becc0a4954b9318e3dd53f047cbc6768a7e
kuba-bogacki <jb.bogacki@gmail.com>
2026-06-17 22:29:12 +0200

    Fix: order create event throws NullPointerException

--- order-service/src/main/java/template/order/domain/Order.java
+++ order-service/src/main/java/template/order/domain/Order.java
@@ -39,7 +39,7 @@ public class Order {
     @Column(nullable = false)
     private DeliveryAddress deliveryAddress;
 
-    @Column(nullable = false)
+    @Builder.Default
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> items = new ArrayList<>();
 
